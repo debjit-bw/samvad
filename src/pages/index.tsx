2699f109656e7a4a54496a7e6773ed6d2f7204d1
Akshay Sharma <codewithakki@gmail.com>
2023-11-25 14:34:25 +0530

    Metamask

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 
 import React from "react";
 import Comments from "../../components/Comments/Comments";
+import Layout from "../../components/layout/layout";
 export default function Home() {
 
   return (
     <>
       <div>
         <h1>Lets Start Building </h1>
+      </div>
+      <div>
+        <Layout/>
       </div>
       <div style={{marginTop:'40px'}}>
        <Comments/>
